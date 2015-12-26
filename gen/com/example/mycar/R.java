3e350400ffab5c5c0188164cf16f0a53bb58b406
gaog1x2l3 <626793098@qq.com>
2015-12-26 09:23:11 +0800

    卡车之家:增加监听网络状态广播

--- gen/com/example/mycar/R.java
+++ gen/com/example/mycar/R.java
@@ -248,18 +248,19 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ptrSubHeaderTextAppearance=0x7f01000b;
     }
     public static final class color {
-        public static final int background_tab_pressed=0x7f08000b;
+        public static final int background_tab_pressed=0x7f080009;
         public static final int black=0x7f080003;
         public static final int black_overlay=0x7f080000;
         public static final int gray_background=0x7f080001;
+        public static final int item_author_textcolor=0x7f08000b;
+        public static final int item_title_textcolor=0x7f08000c;
         public static final int line_color=0x7f080005;
+        public static final int main_rb_background=0x7f08000a;
         public static final int menu_item_text_color=0x7f080008;
-        public static final int news_item_has_read_textcolor=0x7f080009;
-        public static final int news_item_no_read_textcolor=0x7f08000a;
         public static final int red=0x7f080004;
         public static final int search_tab_nomarl=0x7f080006;
         public static final int search_tab_select=0x7f080007;
-        public static final int text_color=0x7f08000c;
+        public static final int text_color=0x7f08000d;
         public static final int white=0x7f080002;
     }
     public static final class dimen {
@@ -274,6 +275,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int indicator_internal_padding=0x7f060002;
         public static final int indicator_right_padding=0x7f060000;
         public static final int list_padding=0x7f060008;
+        public static final int main_rb_text_size=0x7f06000d;
         public static final int shadow_width=0x7f060009;
         public static final int slidingmenu_offset=0x7f060007;
     }
@@ -353,90 +355,92 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int personal_set=0x7f020048;
         public static final int personal_shoutai=0x7f020049;
         public static final int selector_dot=0x7f02004a;
-        public static final int wenzhangguanggaotuiguang=0x7f02004b;
-        public static final int zixun_slc=0x7f02004c;
+        public static final int toast_bg=0x7f02004b;
+        public static final int wenzhangguanggaotuiguang=0x7f02004c;
+        public static final int zixun_slc=0x7f02004d;
     }
     public static final class id {
         public static final int about=0x7f050019;
-        public static final int bankuai=0x7f050023;
+        public static final int bankuai=0x7f050024;
         public static final int bg1=0x7f050012;
         public static final int bg2=0x7f050014;
         public static final int bg3=0x7f050016;
         public static final int bg4=0x7f050018;
         public static final int both=0x7f050003;
-        public static final int caogao=0x7f050036;
-        public static final int car=0x7f05002b;
-        public static final int car_finder=0x7f050028;
+        public static final int caogao=0x7f050037;
+        public static final int car=0x7f05002c;
+        public static final int car_finder=0x7f050029;
         public static final int disabled=0x7f050000;
         public static final int dot_layout=0x7f05001d;
-        public static final int fl_inner=0x7f05004a;
+        public static final int fl_inner=0x7f05004b;
         public static final int fl_kache=0x7f05000c;
         public static final int flip=0x7f050008;
-        public static final int flt_bbs=0x7f050025;
-        public static final int goods=0x7f05002c;
+        public static final int flt_bbs=0x7f050026;
+        public static final int goods=0x7f05002d;
         public static final int gridview=0x7f050009;
-        public static final int haoyou=0x7f050035;
-        public static final int img_xx=0x7f050024;
-        public static final int jinghua=0x7f050020;
-        public static final int ll_nav=0x7f05003b;
-        public static final int mViewPager=0x7f050039;
-        public static final int mViewPager_bbs_jinghua=0x7f050027;
+        public static final int haoyou=0x7f050036;
+        public static final int img_xx=0x7f050025;
+        public static final int jinghua=0x7f050021;
+        public static final int ll_nav=0x7f05003c;
+        public static final int mViewPager=0x7f05003a;
+        public static final int mViewPager_bbs_jinghua=0x7f050028;
         public static final int manualOnly=0x7f050004;
-        public static final int mine_shoutai=0x7f050034;
-        public static final int mine_sixin=0x7f050033;
-        public static final int mine_tongzhi=0x7f050032;
-        public static final int money=0x7f05002a;
+        public static final int mine_shoutai=0x7f050035;
+        public static final int mine_sixin=0x7f050034;
+        public static final int mine_tongzhi=0x7f050033;
+        public static final int money=0x7f05002b;
         public static final int pullDownFromTop=0x7f050005;
         public static final int pullFromEnd=0x7f050002;
         public static final int pullFromStart=0x7f050001;
         public static final int pullUpFromBottom=0x7f050006;
-        public static final int pull_to_refresh_image=0x7f05004b;
-        public static final int pull_to_refresh_progress=0x7f05004c;
-        public static final int pull_to_refresh_sub_text=0x7f05004e;
-        public static final int pull_to_refresh_text=0x7f05004d;
+        public static final int pull_to_refresh_image=0x7f05004c;
+        public static final int pull_to_refresh_progress=0x7f05004d;
+        public static final int pull_to_refresh_sub_text=0x7f05004f;
+        public static final int pull_to_refresh_text=0x7f05004e;
         public static final int rb_1=0x7f05000e;
         public static final int rb_2=0x7f05000f;
         public static final int rb_3=0x7f050010;
         public static final int rb_4=0x7f050011;
-        public static final int recommend=0x7f05002f;
-        public static final int relate_mine=0x7f050030;
-        public static final int retie=0x7f050022;
+        public static final int recommend=0x7f050030;
+        public static final int relate_mine=0x7f050031;
+        public static final int retie=0x7f050023;
         public static final int rg_1=0x7f05000d;
         public static final int rotate=0x7f050007;
-        public static final int sale=0x7f05002e;
+        public static final int sale=0x7f05002f;
         public static final int scrollview=0x7f05000b;
-        public static final int shoucang=0x7f050037;
-        public static final int store=0x7f050029;
-        public static final int tabs=0x7f050038;
-        public static final int tabs_bbs_jinghua=0x7f050026;
+        public static final int shoucang=0x7f050038;
+        public static final int store=0x7f05002a;
+        public static final int tabs=0x7f050039;
+        public static final int tabs_bbs_jinghua=0x7f050027;
         public static final int text1=0x7f050013;
         public static final int text2=0x7f050015;
         public static final int text3=0x7f050017;
         public static final int text4=0x7f05001a;
+        public static final int textView=0x7f05001f;
         public static final int text_4_1=0x7f05001b;
         public static final int text_ad=0x7f05001e;
-        public static final int text_ht=0x7f05003f;
-        public static final int text_mine=0x7f050031;
-        public static final int text_sj=0x7f05003d;
-        public static final int tiezi=0x7f050021;
-        public static final int tiezi_title=0x7f05003c;
-        public static final int toos=0x7f05002d;
-        public static final int tuiguang=0x7f050045;
-        public static final int tuiguang_tt=0x7f050044;
+        public static final int text_ht=0x7f050040;
+        public static final int text_mine=0x7f050032;
+        public static final int text_sj=0x7f05003e;
+        public static final int tiezi=0x7f050022;
+        public static final int tiezi_title=0x7f05003d;
+        public static final int toos=0x7f05002e;
+        public static final int tuiguang=0x7f050046;
+        public static final int tuiguang_tt=0x7f050045;
         public static final int viewPager=0x7f05001c;
-        public static final int web=0x7f05001f;
+        public static final int web=0x7f050020;
         public static final int webview=0x7f05000a;
-        public static final int zixun_img=0x7f050046;
-        public static final int zixun_img_tt=0x7f050040;
-        public static final int zuozhe=0x7f05003e;
-        public static final int zx_Author=0x7f050048;
-        public static final int zx_Author_tt=0x7f050042;
-        public static final int zx_PublishDateTime=0x7f050049;
-        public static final int zx_Title=0x7f050047;
-        public static final int zx_Title_tt=0x7f050041;
-        public static final int zx_head=0x7f05003a;
-        public static final int zx_tg_tt=0x7f050043;
-        public static final int zx_tuijian=0x7f05004f;
+        public static final int zixun_img=0x7f050047;
+        public static final int zixun_img_tt=0x7f050041;
+        public static final int zuozhe=0x7f05003f;
+        public static final int zx_Author=0x7f050049;
+        public static final int zx_Author_tt=0x7f050043;
+        public static final int zx_PublishDateTime=0x7f05004a;
+        public static final int zx_Title=0x7f050048;
+        public static final int zx_Title_tt=0x7f050042;
+        public static final int zx_head=0x7f05003b;
+        public static final int zx_tg_tt=0x7f050044;
+        public static final int zx_tuijian=0x7f050050;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -446,23 +450,28 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ad4=0x7f030004;
         public static final int ad_welcome=0x7f030005;
         public static final int ceshi=0x7f030006;
-        public static final int detail_layout=0x7f030007;
-        public static final int fragment_bbs=0x7f030008;
-        public static final int fragment_bbs_jinghua=0x7f030009;
-        public static final int fragment_dis=0x7f03000a;
-        public static final int fragment_mine=0x7f03000b;
-        public static final int fragment_zixun=0x7f03000c;
-        public static final int headnav=0x7f03000d;
-        public static final int item_tiezi=0x7f03000e;
-        public static final int item_toutiao=0x7f03000f;
-        public static final int item_tuiguang=0x7f030010;
-        public static final int item_zixun_zero=0x7f030011;
-        public static final int pull_to_refresh_header_horizontal=0x7f030012;
-        public static final int pull_to_refresh_header_vertical=0x7f030013;
-        public static final int zixun_tuijian=0x7f030014;
+        public static final int custom_toast=0x7f030007;
+        public static final int detail_layout=0x7f030008;
+        public static final int fragment_bbs=0x7f030009;
+        public static final int fragment_bbs_jinghua=0x7f03000a;
+        public static final int fragment_dis=0x7f03000b;
+        public static final int fragment_mine=0x7f03000c;
+        public static final int fragment_zixun=0x7f03000d;
+        public static final int headnav=0x7f03000e;
+        public static final int item_tiezi=0x7f03000f;
+        public static final int item_toutiao=0x7f030010;
+        public static final int item_tuiguang=0x7f030011;
+        public static final int item_zixun_zero=0x7f030012;
+        public static final int pull_to_refresh_header_horizontal=0x7f030013;
+        public static final int pull_to_refresh_header_vertical=0x7f030014;
+        public static final int zixun_tuijian=0x7f030015;
     }
     public static final class string {
         public static final int app_name=0x7f070006;
+        public static final int bbs=0x7f070008;
+        public static final int discover=0x7f070009;
+        public static final int jump=0x7f07000b;
+        public static final int mine=0x7f070007;
         /**  Just use standard Pull Down String when pulling up. These can be set for languages which require it 
  Just use standard Pull Down String when pulling up. These can be set for languages which require it 
          */
@@ -472,6 +481,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int pull_to_refresh_pull_label=0x7f070000;
         public static final int pull_to_refresh_refreshing_label=0x7f070002;
         public static final int pull_to_refresh_release_label=0x7f070001;
+        public static final int zixun=0x7f07000a;
     }
     public static final class style {
         /** 
@@ -500,6 +510,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f090001;
+        public static final int style_rb_btn=0x7f090004;
         public static final int style_title=0x7f090003;
         public static final int style_title_btn=0x7f090002;
     }
